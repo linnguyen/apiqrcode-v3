cd30b0f14331d4113eaa82707b1a67ff7e9d5f9c
linnguyen <nguyenvanlin01041994@gmail.com>
2017-04-17 16:29:48 +0700

    add login / logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = NhanVien.find_by(ten_dang_nhap: params[:session][:ten_dang_nhap])
+  	if user 
+  		log_in user
+  		redirect_to root_path
+  	else
+  		flash.now[:danger] = 'User / Password chưa đúng'
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	 log_out
+  	 redirect_to  sessions_new_path
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+
+	def log_in(user)
+       session[:ma_nql] = user.id
+	end
+
+	def current_user
+		@current_user ||= NhanVien.find_by(ma_nql: session[:ma_nql])
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+    
+    def log_out
+    	session.delete(:ma_nql)
+    	@current_user = nil
+    end
 end
