8fe7b1e4aaaa295eba6389d7af6a65ab0836e2b0
linnguyen <nguyenvanlin01041994@gmail.com>
2017-05-08 10:48:28 +0700

    statistic by year

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,13 +1,14 @@
 class StatisticsController < ApplicationController
 	def show_by_time
-         time = params[:time]
-		 month = params[:time][0..1]
-		 year = params[:time][4..7]
-         @thiet_bi = KiemKe.where("MONTH(thoi_gian) = ? && YEAR(thoi_gian) = ?", month, year)
-         .paginate(page: params[:page], :per_page => 8)
+         @year = params[:time]
+         @thiet_bi = KiemKe.where("YEAR(thoi_gian) = ?", @year).paginate(page: params[:page], :per_page => 8)
     end
 
 	def show_by_room
-          @thiet_bi = KiemKe.where("ma_pth = ?", params[:room]).paginate(page: params[:page], :per_page => 8)
+         @thiet_bi = KiemKe.where("ma_pth = ?", params[:room]).paginate(page: params[:page], :per_page => 8)
+	end
+
+	def show_by_inventory_season
+		 @thiet_bi = KiemKe.where("ma")
 	end
 end
