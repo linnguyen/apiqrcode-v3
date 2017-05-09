3c975c951bb58b2055a9b5cb5061bb5b17090bf4
linnguyen <nguyenvanlin01041994@gmail.com>
2017-05-09 17:19:19 +0700

    add more validation

--- app/controllers/api/device_informations_controller.rb
+++ app/controllers/api/device_informations_controller.rb
@@ -1,10 +1,14 @@
 class Api::DeviceInformationsController < ApplicationController
 	def show_info
          chi_tiet_phieu_xuat =  ChiTietThietBiXuat.find_by(ma_qr: params[:qrcode])
-         phieu_xuat =  chi_tiet_phieu_xuat.phieu_xuat
-         thiet_bi = phieu_xuat.chi_tiet_nhap_xuat.find_by(ma_code: params[:qrcode][0..7]) # -> one device
-         nhan_vien = phieu_xuat.nhan_vien
-         render json: {:staff => nhan_vien, :device => thiet_bi}
+         if chi_tiet_phieu_xuat.nil?
+         	 render json: {:message => "Không có dữ liệu cho mã QR này!"}
+         else
+         	 phieu_xuat =  chi_tiet_phieu_xuat.phieu_xuat
+             thiet_bi = phieu_xuat.chi_tiet_nhap_xuat.find_by(ma_code: params[:qrcode][0..7]) # -> one device
+             nhan_vien = phieu_xuat.nhan_vien
+             render json: {:staff => nhan_vien, :device => thiet_bi}
+         end
          # optimize code
     end
 end
