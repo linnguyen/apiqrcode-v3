20efa47b3a891fa08eef46bd324a5ce9c9b8fa92
linnguyen <nguyenvanlin01041994@gmail.com>
2017-05-11 12:52:09 +0700

    heroku

--- app/models/de_xuat_phoi_lieu.rb
+++ app/models/de_xuat_phoi_lieu.rb
@@ -1,6 +1,6 @@
 class DeXuatPhoiLieu < ApplicationRecord
 	self.table_name = "tb_de_xuat_phoi_lieu"
-	default_scope -> { order(thoi_gian: :desc)
+	default_scope -> { order(thoi_gian: :desc) }
 	has_many :chi_tiet_de_xuat_phoi_lieu , foreign_key: "ma_dxpl", class_name: "ChiTietDeXuatPhoiLieu", dependent: :destroy
     belongs_to :nhan_vien , foreign_key: "ma_nql", class_name: "NhanVien"
     
