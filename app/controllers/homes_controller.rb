d9986bf159f4101b81d2978f1009ed9bce417648
linnguyen <nguyenvanlin01041994@gmail.com>
2017-05-11 12:49:32 +0700

    heroku

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+	before_action :logged_in_user, only: [:index]
     def index
     	# render json: get_list_day
     	# byebug
--- app/models/de_xuat_phoi_lieu.rb
+++ app/models/de_xuat_phoi_lieu.rb
@@ -1,8 +1,9 @@
 class DeXuatPhoiLieu < ApplicationRecord
 	self.table_name = "tb_de_xuat_phoi_lieu"
+	default_scope -> { order(thoi_gian: :desc)
 	has_many :chi_tiet_de_xuat_phoi_lieu , foreign_key: "ma_dxpl", class_name: "ChiTietDeXuatPhoiLieu", dependent: :destroy
     belongs_to :nhan_vien , foreign_key: "ma_nql", class_name: "NhanVien"
-
+    
     # enum trang_thai: [:dang_cho, :da_xu_li]
     # enum xac_nhan: [:non_ok, :ok]
 
