da86ba9b97061af443fe4b23cd8f6bfc0c2f296a
linnguyen <nguyenvanlin01041994@gmail.com>
2017-05-23 09:02:26 +0700

    delete debug

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 	before_action :logged_in_user, only: [:index]
     def index
-    	d = get_amount_of_output_device_by_room 'D110','TB00001'
-    	byebug
+    	# d = get_amount_of_output_device_by_room 'D110','TB00001'
+    	# byebug
     end
 end
